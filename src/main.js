627192e218057f3e042678a6fa99c5311b262482
Ange <148551243+20ange20@users.noreply.github.com>
2025-11-22 13:27:17 -0300

    funciona o básico

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-
-createApp(App).mount('#app')
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-
-
-
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,15 @@ import Home from '../views/Home.vue'
 import Sobre from '../views/Sobre.vue'
 import ProjectDetail from '../views/ProjectDetail.vue'
 
-
 const routes = [
-{ path: '/', name: 'Home', component: Home },
-{ path: '/sobre', name: 'Sobre', component: Sobre },
-{ path: '/projeto/:id', name: 'ProjectDetail', component: ProjectDetail, props: true }
+  { path: '/', name: 'Home', component: Home },
+  { path: '/sobre', name: 'Sobre', component: Sobre },
+  { path: '/projetos/:id', name: 'ProjectDetail', component: ProjectDetail }
 ]
 
-
 const router = createRouter({
-history: createWebHistory(),
-routes
+  history: createWebHistory(),
+  routes,
 })
-
 
 export default router
